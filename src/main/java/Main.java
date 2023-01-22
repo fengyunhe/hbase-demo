97c0b156980db3c1bce49578d5a3b21c0d103c0d
yan.yang <277160299@qq.com>
2023-01-22 08:43:33 +0800

    #1 feat: add/delete row/delete column value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,13 +19,61 @@ public class Main {
         Configuration conf = getConfiguration();
         createSchemaTables(conf);
         modifyColumns(conf);
+        addData(conf);
+    }
+
+    private static void addData(Configuration conf) throws IOException {
+        try (Connection connection = ConnectionFactory.createConnection(conf)) {
+            Table demoTable = connection.getTable(TableName.valueOf("user2"));
+            byte[] infos = Bytes.toBytes("info");
+            byte[] statuses = Bytes.toBytes("status");
+            demoTable.put(List.of(
+                    new Put("1".getBytes()).addColumn(infos,
+                            Bytes.toBytes("address"), Bytes.toBytes("延长县" + RandomUtils.nextLong())),
+                    new Put("1".getBytes()).addColumn(infos,
+                            Bytes.toBytes("name"), Bytes.toBytes("xiaoming" + RandomUtils.nextLong())),
+                    new Put("1".getBytes()).addColumn(infos,
+                            Bytes.toBytes("age"), Bytes.toBytes("1")),
+                    new Put("1".getBytes()).addColumn(statuses,
+                            Bytes.toBytes("live"), Bytes.toBytes("suspend")),
+                    new Put("1".getBytes()).addColumn(statuses,
+                            Bytes.toBytes("award"), Bytes.toBytes("2023")),
+
+                    new Put("2".getBytes()).addColumn(infos,
+                            Bytes.toBytes("address"), Bytes.toBytes("延长县" + RandomUtils.nextLong())),
+                    new Put("2".getBytes()).addColumn(infos,
+                            Bytes.toBytes("name"), Bytes.toBytes("xiaoming" + RandomUtils.nextLong())),
+                    new Put("2".getBytes()).addColumn(infos,
+                            Bytes.toBytes("age"), Bytes.toBytes("1")),
+                    new Put("2".getBytes()).addColumn(statuses,
+                            Bytes.toBytes("live"), Bytes.toBytes("suspend")),
+
+                    new Put("3".getBytes()).addColumn(infos,
+                            Bytes.toBytes("address"), Bytes.toBytes("延长县" + RandomUtils.nextLong())),
+                    new Put("3".getBytes()).addColumn(infos,
+                            Bytes.toBytes("name"), Bytes.toBytes("xiaoming" + RandomUtils.nextLong())),
+                    new Put("3".getBytes()).addColumn(infos,
+                            Bytes.toBytes("age"), Bytes.toBytes("3")),
+                    new Put("3".getBytes()).addColumn(statuses,
+                            Bytes.toBytes("live"), Bytes.toBytes("suspend")),
+                    new Put("3".getBytes()).addColumn(statuses,
+                            Bytes.toBytes("award"), Bytes.toBytes("2023"))
+                    ));
+            demoTable.mutateRow(
+                    RowMutations.of(List.of(
+                            new Delete("2".getBytes())
+                    ))
+            );
+            demoTable.delete(new Delete("1".getBytes()).addColumn(statuses, "award".getBytes()));
+        }
     }
 
     private static Configuration getConfiguration() {
         Configuration conf = new Configuration();
-        conf.addResource(Thread.currentThread().getContextClassLoader().getResource("core-site.xml"));
-        conf.addResource(Thread.currentThread().getContextClassLoader().getResource("hdfs-site.xml"));
-        conf.addResource(Thread.currentThread().getContextClassLoader().getResource("hbase-site.xml"));
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        conf.addResource(contextClassLoader.getResource("core-site.xml"));
+        conf.addResource(contextClassLoader.getResource("hdfs-site.xml"));
+        conf.addResource(contextClassLoader.getResource("hbase-site.xml"));
         return conf;
     }
 
@@ -51,7 +101,7 @@ public class Main {
                             admin.getDescriptor(user2).getColumnFamily("info".getBytes(StandardCharsets.UTF_8))
                     ).setMaxVersions(5).build());
 //            admin.enableTable(user2);
-            admin.addColumnFamily(user2,ColumnFamilyDescriptorBuilder.of("status"));
+            admin.addColumnFamily(user2, ColumnFamilyDescriptorBuilder.of("status"));
             log.info("Modify table Done. ");
         }
     }
